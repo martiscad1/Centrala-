0398d11ef52961a6754da90a2b6b97fc417d5d99
martiscad16 <martiscad16@gmail.com>
2025-11-23 19:18:50 +0000

    feat: Ajustare design header și eliminare Service Worker

--- src/main.jsx
+++ src/main.jsx
@@ -20,17 +20,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       pauseOnFocusLoss
       draggable
       pauseOnHover
-      theme="colored" // Va folosi culorile definite de noi în CSS
+      theme="colored"
     />
   </React.StrictMode>
 );
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js').then(registration => {
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, err => {
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
